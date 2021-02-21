688ed23e7e3cf9ee8d59e4c4655230900fde7554
root <root@DESKTOP-T18E2AR.localdomain>
2021-02-21 05:09:44 +0000

    modified:   train_voc.sh

--- train_voc.sh
+++ train_voc.sh
@@ -3,9 +3,11 @@ python train.py \
 	--backbone resnet \
 	--lr 0.007 \
 	--workers 4 \
-	--epochs 50 \
-	--batch-size 50 \
+	--epochs 2 \
+	--batch-size 5 \
 	--gpu-ids 0 \
 	--checkname deeplab-resnet \
 	--eval-interval 1 \
 	--dataset pascal \
+	--workers 0 \
+	--no-cuda
